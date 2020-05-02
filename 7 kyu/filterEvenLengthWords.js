094926f17e50e013377e168e8dec8f8c7dc4f5d7
Viktoryia A <alekseevavikt@gmail.com>
2020-05-01 21:07:33 -0700

    filterEvenLengthWords

---
+++ 7 kyu/filterEvenLengthWords.js
@@ -0,0 +1,17 @@
+/*
+https://www.codewars.com/kata/59564f3bcc15b5591a00004a/train/javascript
+
+Description:
+Write a function called "filterEvenLengthWords".
+
+Given an array of strings, "filterEvenLengthWords" returns an array containing only the elements of the given array whose length is an even number.
+
+var output = filterEvenLengthWords(['word', 'words', 'word', 'words']);
+
+console.log(output); // --> ['word', 'word']
+
+FUNDAMENTALS
+ */
+function filterEvenLengthWords(words) {
+    return words.filter(el => el.length % 2 === 0);
+}
