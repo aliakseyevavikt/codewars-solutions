05a621b51ab5fcf2d1ba2705ce8aea7457468f4b
Viktoryia A <alekseevavikt@gmail.com>
2020-05-05 22:47:42 -0700

    Grasshopper - Combine strings

---
+++ 8 kyu/Grasshopper - Combine strings.js
@@ -0,0 +1,19 @@
+/*
+https://www.codewars.com/kata/55f73f66d160f1f1db000059/javascript
+
+Description:
+Combine strings function
+Create a function named combineNames(combine_names in python, ruby, CombineNames in C#) that accepts two parameters
+(first and last name). The function should return the full name.
+
+Example:
+
+combineNames('James', 'Stevens')
+returns:
+
+'James Stevens'
+ */
+
+function combineNames(first, last) {
+    return `${first} ${last}`;
+}
