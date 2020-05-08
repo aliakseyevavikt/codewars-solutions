2954d5412c22ea784595e1ddd4f99660f6c051fd
Viktoryia A <alekseevavikt@gmail.com>
2020-05-08 13:58:47 -0700

    Grasshopper - Create the rooms

---
+++ 8 kyu/Grasshopper - Create the rooms.js
@@ -0,0 +1,27 @@
+/*
+https://www.codewars.com/kata/56a29b237e9e997ff2000048
+
+Description:
+Escape the room
+You are creating an escape the room game. The first step is create an object called rooms that contains all of the
+ rooms of the game. The room object should have at least 3 rooms as properties, and each individial room
+should have at least three properties (example: name, description, completed).
+ */
+
+var rooms = {
+    room1: {
+        name: 'room1',
+        description: 'Description1',
+        completed: true
+    },
+    room2: {
+        name: 'room2',
+        description: 'Description2',
+        completed: false
+    },
+    room3: {
+        name: 'room3',
+        description: 'Description3',
+        completed: true
+    }
+}
