6714ea582785ed0890018848c157bb89125cfe23
Viktoryia A <alekseevavikt@gmail.com>
2020-05-08 14:11:28 -0700

    What's wrong with these identifiers?.js

---
+++ 8 kyu/What's wrong with these identifiers?.js
@@ -0,0 +1,14 @@
+/*
+https://www.codewars.com/kata/56bb01de0e8b29de50000b19
+
+Description:
+An identifier is simply a name...
+Can you amend this object so that its properties comprise only vaild identifiers?
+ */
+
+var Person = {
+    firstName: "John",
+    secondName: "Doe",
+    email: "john.doe@email.com",
+    gender: "M"
+};
