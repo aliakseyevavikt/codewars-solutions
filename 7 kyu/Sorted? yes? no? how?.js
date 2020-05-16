c3a2fb78e79719acab9f1f3fa772c6d1e8797b15
Viktoryia A <alekseevavikt@gmail.com>
2020-05-16 12:57:41 -0700

    Sorted?

--- 7 kyu/Sorted? yes? no? how?.js
+++ 7 kyu/Sorted? yes? no? how?.js
@@ -9,3 +9,13 @@ Complete the method which accepts an array of integers, and returns one of the f
 "no" - otherwise
 You can assume the array will always be valid, and there will always be one correct answer.
  */
+
+function isSortedAndHow(arr) {
+    let asc = 0, desc = 0;
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] <= arr[i + 1]) asc++;
+        if (arr[i] >= arr[i + 1]) desc++;
+    }
+    return asc === arr.length - 1 ? "yes, ascending" : desc === arr.length - 1 ? "yes, descending" :
+        "no";
+}
