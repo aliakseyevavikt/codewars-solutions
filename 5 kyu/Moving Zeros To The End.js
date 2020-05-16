5e947720af1d48d36c5cc745f93a76b97c4ebefa
Viktoryia A <alekseevavikt@gmail.com>
2020-05-16 13:19:13 -0700

    Moving Zeros To The End.js

---
+++ 5 kyu/Moving Zeros To The End.js
@@ -0,0 +1,20 @@
+/*
+https://www.codewars.com/kata/52597aa56021e91c93000cb0
+
+Description:
+Write an algorithm that takes an array and moves all of the zeros to the end,
+preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+ */
+
+var moveZeros = function (arr) {
+    let res = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== 0) res.push(arr[i]);
+    }
+    while (res.length !== arr.length) {
+        res.push(0);
+    }
+    return res;
+}
