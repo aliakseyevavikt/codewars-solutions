7a2a1a4fd288d4eec9f05f6ec57f908a7607f8ba
Viktoryia A <alekseevavikt@gmail.com>
2020-05-15 22:27:31 -0700

    Multiplication table.js

---
+++ 6 kyu/Multiplication table.js
@@ -0,0 +1,25 @@
+/*
+https://www.codewars.com/kata/534d2f5b5371ecf8d2000a08/javascript
+
+Description:
+Your task, is to create NxN multiplication table, of size provided in parameter.
+
+for example, when given size is 3:
+
+1 2 3
+2 4 6
+3 6 9
+for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
+ */
+
+multiplicationTable = function (size) {
+    let arr = [];
+    for (let i = 1; i <= size; i++) {
+        let temp = [];
+        for (let j = i; j <= size * i; j = j + i) {
+            temp.push(j);
+        }
+        arr.push(temp);
+    }
+    return arr;
+}
