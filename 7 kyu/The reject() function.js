98a61bb83bd673ffae69c7d3ece747da0854aca5
Viktoryia A <alekseevavikt@gmail.com>
2020-06-16 23:11:18 -0700

    The reject

---
+++ 7 kyu/The reject() function.js
@@ -0,0 +1,12 @@
+/*
+https://www.codewars.com/kata/52988f3f7edba9839c00037d/train/javascript
+
+Description:
+Implement a function which filters out array values which satisfy the given predicate.
+
+reject([1, 2, 3, 4, 5, 6], (n) => n % 2 === 0)  =>  [1, 3, 5]
+ */
+
+function reject(array, predicate) {
+    return array.filter(n => !predicate(n));
+}
