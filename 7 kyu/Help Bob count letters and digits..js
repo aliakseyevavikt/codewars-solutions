50d8c7423f92e0c43d3b962674b326abbd532a5a
Viktoryia A <alekseevavikt@gmail.com>
2020-06-16 23:15:20 -0700

    Help Bob

---
+++ 7 kyu/Help Bob count letters and digits..js
@@ -0,0 +1,20 @@
+/*
+https://www.codewars.com/kata/5738f5ea9545204cec000155/train/javascript
+
+Description:
+Bob is a lazy man.
+
+He needs you to create a method that can determine how many letters and digits are in a given string.
+
+Example:
+
+"hel2!lo" --> 6
+
+"wicked .. !" --> 6
+
+"!?..A" --> 1
+ */
+
+function countLettersAndDigits(input) {
+    return input.replace(/[^a-z0-9]/gi, '').length;
+}
