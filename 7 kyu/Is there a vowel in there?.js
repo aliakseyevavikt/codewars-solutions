1f0752134bcab9dc9a73015cd60569522336a5e1
Viktoryia A <alekseevavikt@gmail.com>
2020-06-17 23:40:45 -0700

    Is there a vowel

---
+++ 7 kyu/Is there a vowel in there?.js
@@ -0,0 +1,18 @@
+/*
+https://www.codewars.com/kata/57cff961eca260b71900008f/train/javascript
+
+Description:
+Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
+
+If they are, change the array value to a string of that vowel.
+
+Return the resulting array.
+ */
+
+function isVow(a) {
+    for (let i = 0; i < a.length; i++) {
+        let char = String.fromCharCode(a[i]);
+        if (/[aeoiu]/.test(char)) a[i] = char;
+    }
+    return a;
+}
