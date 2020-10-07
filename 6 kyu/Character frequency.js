8e163af0934355f3abe394e348e5029b1af18b40
Viktoryia A <alekseevavikt@gmail.com>
2020-10-07 15:55:15 -0700

    Character frequency.js

---
+++ 6 kyu/Character frequency.js
@@ -0,0 +1,31 @@
+/*
+https://www.codewars.com/kata/53e895e28f9e66a56900011a/train/javascript
+
+Description:
+Write a function that takes a piece of text in the form of a string and returns the letter frequency count for the text.
+ This count excludes numbers, spaces and all punctuation marks. Upper and lower case versions of a character
+  are equivalent and the result should all be in lowercase.
+
+The function should return a list of tuples (in Python and Haskell) or arrays (in other languages) sorted by
+the most frequent letters first. The Rust implementation should return an ordered BTreeMap. Letters with
+the same frequency are ordered alphabetically. For example:
+
+  letterFrequency('aaAabb dddDD hhcc')
+will return
+
+  [['d',5], ['a',4], ['b',2], ['c',2], ['h',2]]
+Letter frequency analysis is often used to analyse simple substitution cipher texts like those created by the Caesar cipher.
+ */
+
+function letterFrequency(text) {
+    let obj = {};
+    text = text.toLowerCase().replace(/[^a-z]/g, '').split('').sort();
+    for (let i = 0; i < text.length; i++) {
+        if (obj.hasOwnProperty(text[i])) {
+            obj[text[i]]++
+        } else {
+            obj[text[i]] = 1;
+        }
+    }
+    return Object.entries(obj).sort((a, b) => b[1] - a[1] || a[0].localeCompare(b[0]));
+}
