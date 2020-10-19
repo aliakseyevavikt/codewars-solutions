ee4658f2d92abb8a98b062c4f6156469acaacc66
Viktoryia A <alekseevavikt@gmail.com>
2020-10-18 22:38:23 -0700

    Minimum Steps (Array Series #6).js

---
+++ 7 kyu/Minimum Steps (Array Series #6).js
@@ -0,0 +1,42 @@
+/*
+https://www.codewars.com/kata/5a91a7c5fd8c061367000002/train/javascript
+
+Description:
+Task
+Given an array of N integers, you have to find how many times you have to add up the smallest numbers
+ in the array until their Sum becomes greater or equal to K.
+
+Notes:
+List size is at least 3.
+
+All numbers will be positive.
+
+Numbers could occur more than once , (Duplications may exist).
+
+Threshold K will always be reachable.
+
+Input >> Output Examples
+minimumSteps({1, 10, 12, 9, 2, 3}, 6)  ==>  return (2)
+Explanation:
+We add two smallest elements (1 + 2), their sum is 3 .
+
+Then we add the next smallest number to it (3 + 3) , so the sum becomes 6 .
+
+Now the result is greater or equal to 6 , Hence the output is (2) i.e (2) operations are required to do this.
+
+ */
+
+function minimumSteps(numbers, value) {
+    numbers = numbers.sort((a, b) => a - b);
+    let count = 0;
+    let sum = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        if (sum >= value) {
+            return count;
+        } else {
+            sum += numbers[i];
+            count++;
+        }
+    }
+    return count;
+}
